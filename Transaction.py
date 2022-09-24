86428868625ca415c73edbdab736d4639ae5399b
Haiden <haiden.m.smith02@gmail.com>
2022-09-23 21:24:00 -0400

    Created transaction entity

---
+++ Transaction.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# setting up SQLAlchemy and data models so
+# we can map data models into database tables
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+
+class Transaction(db.Model):
+    # Transaction ID
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    # ID of the user renting the property.
+    id_renter = db.Column(db.Integer, unique=True, nullable=False)
+    # ID of the user listing their property to be rented.
+    id_lister = db.Column(db.Integer, unique=True, nullable=False)
+    # Listing price
+    listing_price = db.Column(db.Integer,
+                              db.ForeignKey('Listing.price'), nullable=False)
+    # listing availability.
+    listing_availability = db.Column(
+        db.Boolean, db.ForeignKey('Listing.availability'), nullable=False)
+    # Relationship to listing entity.
+    listing = db.relationship('Listing', backref='listing', lazy=True)
+
+    def __repr__(self):
+        """Each listing will be represented by a unique ID"""
+        return '<Transaction %r>' % self.id
