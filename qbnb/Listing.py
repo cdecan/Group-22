c3077b409362512d17fc123362f4185afb6f9aba
Connor <connordecan@gmail.com>
2022-10-03 22:38:17 -0400

    Updated listing entity to fit the new criteria and edited main to include external files

--- qbnb/Listing.py
+++ qbnb/Listing.py
@@ -18,9 +18,10 @@ class Listing(db.Model):
     description = db.Column(db.String, nullable=True)
     price = db.Column(db.Float, nullable=False)
     last_modified_date = db.Column(db.Date, nullable=False)
-    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
-    owner = db.relationship('Listing', backref=db.backref('listings'
-                                                          , lazy=True))
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
+                         nullable=False)
+    owner = db.relationship('Listing', backref=db.backref('listings',
+                                                          lazy=True))
 
     def __repr__(self):
         """A listing represents itself by displaying the associated title"""
