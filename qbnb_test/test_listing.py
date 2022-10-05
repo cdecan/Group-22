ae90c65af839a9ab3de00c306ef9a5762b26afdc
Connor <connordecan@gmail.com>
2022-10-05 18:50:35 -0400

    imported User into the listing test cases

--- qbnb_test/test_listing.py
+++ qbnb_test/test_listing.py
@@ -1,4 +1,5 @@
 from qbnb.listing import create_listing
+from qbnb.User import User
 
 
 def test_r4_1_create_listing():
@@ -6,36 +7,49 @@ def test_r4_1_create_listing():
     Testing R4-1: If title is not alphanumeric or has spaces at the front or back, then it fails.
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1','Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'Title2', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'Title!', 'DescriptionDescription', 20.00) is False
-    assert create_listing('1', 'Title ', 'DescriptionDescription', 20.00) is False
-    assert create_listing('1', ' Title', 'DescriptionDescription', 20.00) is False
-    assert create_listing('1', ' Title ', 'DescriptionDescription', 20.00) is False
+    user = User.user(user_id=1, email="test@test.com")
+    assert create_listing('1','Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'Title2', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'Title!', 'DescriptionDescription', 20.00)\
+           is False
+    assert create_listing('1', 'Title ', 'DescriptionDescription', 20.00)\
+           is False
+    assert create_listing('1', ' Title', 'DescriptionDescription', 20.00)\
+           is False
+    assert create_listing('1', ' Title ', 'DescriptionDescription', 20.00)\
+           is False
 
 
 def test_r4_2_create_listing():
     '''
-    Testing R4-2: If the title of the product is longer than 80 characters then it fails.
+    Testing R4-2: If the title of the product is longer than 80 characters
+     then it fails.
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'Titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-                               'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'Titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                               'eeeeee'
+                               'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+                               'eeeeeeeeeeeeee',
                           'Descriptionnnnnnnnnnnnnnnnnnnnnnnnnnnnn'
-                          'nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
+                          'nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'
+                          'nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
                           20.00) is False
 
 
 def test_r4_3_create_listing():
     '''
-    Testing R4-3: If the description has under 20 or over 2000 characters, then it fails
+    Testing R4-3: If the description has under 20 or over 2000 characters,
+     then it fails
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
     assert create_listing('1', 'Title2', 'Description', 20.00) is False
     longword = "a"
     for i in range(3000):
@@ -49,9 +63,10 @@ def test_r4_4_create_listing():
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'TitleTitleTitleTitleTitleTitleTitleTitleTitle',
-                          'DescriptionDescription', 20.00) is False
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'TitleTitleTitleTitleTitleTitleTitleTitleTitle'
+                          , 'DescriptionDescription', 20.00) is False
 
 
 def test_r4_5_create_listing():
@@ -60,18 +75,23 @@ def test_r4_5_create_listing():
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'Title2', 'DescriptionDescription', 9.00) is False
-    assert create_listing('1', 'Title3', 'DescriptionDescription', 10001.00) is False
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'Title2', 'DescriptionDescription', 9.00)\
+           is False
+    assert create_listing('1', 'Title3', 'DescriptionDescription', 10001.00)\
+           is False
 
 
 def test_r4_6_create_listing():
     '''
-    Testing R4-6: If last_modified_date is not after 2021-01-02 and before 2025-01-02, it fails
+    Testing R4-6: If last_modified_date is not after 2021-01-02
+     and before 2025-01-02, it fails
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
 
 
 def test_r4_7_create_listing():
@@ -81,9 +101,12 @@ def test_r4_7_create_listing():
 
     user = User(user_id=1, email="test@test.com")
     user2 = User(user_id=2, email="")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('2', 'Title2', 'DescriptionDescription', 20.00) is False
-    assert create_listing('3', 'Title3', 'DescriptionDescription', 20.00) is False
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('2', 'Title2', 'DescriptionDescription', 20.00)\
+           is False
+    assert create_listing('3', 'Title3', 'DescriptionDescription', 20.00)\
+           is False
 
 
 def test_r4_8_create_listing():
@@ -92,5 +115,7 @@ def test_r4_8_create_listing():
     '''
 
     user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is True
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00) is False
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is True
+    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+           is False
