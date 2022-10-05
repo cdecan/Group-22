8a1ea00bc7ddeaed717446060969612f504bc1a0
Haiden <haiden.m.smith02@gmail.com>
2022-10-04 21:50:08 -0400

    Changed transaction entity to booking entity and added the required attributes

---
+++ qbnb/Booking.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# setting up SQLAlchemy and data models so
+# we can map data models into database tables
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+
+class Booking(db.Model):
+    """
+    Contains information required to book a property.
+    """
+    
+    # Booking ID
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    # ID of the user making the booking.
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    # ID of the listing to be booked.
+    listing_id = db.Column(db.Integer, unique=True, nullable=False)
+    # Listing price dollars
+    listing_price_dollars = db.Column(
+        db.Integer, db.ForeignKey('Listing.price_dollars'), nullable=False)
+    # Listing price cents
+    listing_price_cents = db.Column(
+        db.Integer, db.ForeignKey('Listing.price_cents'), nullable=False)
+    # Booking date.
+    date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        """Each booking will be represented by a unique ID"""
+        return '<Booking %r>' % self.id
