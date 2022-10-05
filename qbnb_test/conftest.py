398a48828ca25c7bd75fce4e2270c72c4d183240
Connor <connordecan@gmail.com>
2022-10-05 15:48:40 -0400

    Added test code fix

--- qbnb_test/conftest.py
+++ qbnb_test/conftest.py
@@ -10,7 +10,6 @@ This file defines what to do BEFORE running any test cases:
 
 
 def pytest_sessionstart():
-    with app.app_context():
         '''
         Delete database file if existed. So testing can start fresh.
         '''
@@ -18,10 +17,10 @@ def pytest_sessionstart():
         db_file = 'db.sqlite'
         if os.path.exists(db_file):
             os.remove(db_file)
+        app.app_context().push()
 
 
 def pytest_sessionfinish():
-    with app.app_context():
         '''
         Optional function called when testing is done.
         Do nothing for now
