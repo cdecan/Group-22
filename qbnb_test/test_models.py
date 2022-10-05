ddc3f4c68eff0b23eec3893ec964bf8299f51092
Connor <connordecan@gmail.com>
2022-10-05 15:22:32 -0400

    template

--- qbnb_test/test_models.py
+++ qbnb_test/test_models.py
@@ -8,9 +8,9 @@ def test_r1_7_user_register():
         Testing R1-7: If the email has been used, the operation failed.
         '''
 
-        #assert register('u0', 'test0@test.com', '123456') is True
-        #assert register('u0', 'test1@test.com', '123456') is True
-        #assert register('u1', 'test0@test.com', '123456') is False
+        assert register('u0', 'test0@test.com', '123456') is True
+        assert register('u0', 'test1@test.com', '123456') is True
+        assert register('u1', 'test0@test.com', '123456') is False
 
 
 def test_r2_1_login():
@@ -23,10 +23,10 @@ def test_r2_1_login():
           u1 in database)
         '''
 
-        #user = login('test0@test.com', 123456)
+        user = login('test0@test.com', 123456)
 
-        #assert user is not None
-        #assert user.username == 'u0'
+        assert user is not None
+        assert user.username == 'u0'
 
-        #user = login('test0@test.com', 1234567)
-        #assert user is None
+        user = login('test0@test.com', 1234567)
+        assert user is None
