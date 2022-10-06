7435b2877bd9c7a799f7d6c364586e28142e3407
genglin6688 <genglin666888@gmail.com>
2022-10-06 15:21:47 -0400

    made register and login function, also wrote testing code for user.py

--- qbnb/user.py
+++ qbnb/user.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 # setting up SQLAlchemy and data models
 # so we can map data models into database tables
@@ -20,13 +21,95 @@ class User(db.Model):
     # password of user, cannot be null
     password = db.Column(db.String(120), nullable=False)
     # username of user, cannot be null
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    # billing_address, null only for sprint 2
-    billing_address = db.Column(db.String(120))
-    # postal code of user, null only for sprint 2
-    postal_code = db.Column(db.String(120))
-    # balance of user, cannot be null
-    balance = db.Column(db.Integer)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    # billing_address, cannot be null but can be ""
+    billing_address = db.Column(db.String(120), nullable=False)
+    # postal code of user, cannot be null but can be ""
+    postal_code = db.Column(db.String(120), nullable=False)
+    # balance of user, cannot be null but can be 0
+    balance = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return '<User %s>' % self.id
+
+
+# to fix the bug with app.app_context
+app.app_context().push()
+# drop previous table if exists
+# makes sure that no previous data remaining to mess with testing
+# will be removed later when actual data is needed
+db.drop_all()
+# create table
+db.create_all()
+
+
+def register(name, email, password):
+    """
+    Register a new user, template is copied from models.py
+      Parameters:
+        name (string):     user name
+        email (string):    user email
+        password (string): user password
+      Returns:
+        True if registration succeeded otherwise False
+    """
+    # check if email or password is empty
+    if len(email) == 0 or len(password) == 0:
+        return False
+
+    # validating the email using regex,
+    # RFC 5322 is too hard to validate because of the use of "" in address,
+    # so this is a more universal validation
+    regex_email = re.compile(r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+"
+                             r"(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)"
+                             r"*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)"
+                             r"+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")
+    if not re.fullmatch(regex_email, email):
+        return False
+
+    # validating the password using regex
+    regex_password = re.compile("^(?=.*[a-z])(?=." +
+                                "*[A-Z])(?=.*\\d)" +
+                                "(?=.*[-+_!@#$%^&*., ?]).+$")
+    if not (len(password) > 6 and re.search(regex_password, password)):
+        return False
+
+    # validating the username
+    if len(name) < 2 or len(name) > 20 or name[0] == " " or name[-1] == " ":
+        return False
+    # to validate that the name without spaces is all alphanumeric
+    temp_name = name.replace(" ", "")
+    if not temp_name.isalnum():
+        return False
+
+    # check if the email has been used:
+    existed = User.query.filter_by(email=email).all()
+    if len(existed) > 0:
+        return False
+
+    # create a new user
+    user = User(username=name, email=email, password=password,
+                billing_address="", postal_code="", balance=100)
+
+    # add it to the current database session
+    db.session.add(user)
+
+    # actually save the user object
+    db.session.commit()
+
+    return True
+
+
+def login(email, password):
+    """
+    Check login information, this is copied from models.py
+      Parameters:
+        email (string):    user email
+        password (string): user password
+      Returns:
+        The user object if login succeeded otherwise None
+    """
+    valid = User.query.filter_by(email=email, password=password).all()
+    if len(valid) != 1:
+        return None
+    return valid[0]
---
+++ qbnb_test/test_user.py
@@ -0,0 +1,85 @@
+from qbnb.user import register, login
+
+
+def test_r1_7_user_register():
+    """
+    Testing if the register function works
+    """
+    # testing for username in this block
+    assert register('u0', 'user0@test.com', 'Test123_') is True
+    assert register('u1', 'user1@test.com', 'Test123_') is True
+    # illegal character
+    assert register('u0.', 'test2@test.com', 'Test123_') is False
+    # space as a prefix
+    assert register(' u0', 'test2@test.com', 'Test123_') is False
+    # space as a suffix
+    assert register('u0 ', 'test2@test.com', 'Test123_') is False
+    # space as a prefix and suffix
+    assert register(' u0 ', 'test2@test.com', 'Test123_') is False
+    # empty name
+    assert register('', 'test2@test.com', 'Test123_') is False
+    # name too short
+    assert register('X', 'test2@test.com', 'Test123_') is False
+    # name too long
+    assert register('this is a very long name that the '
+                    'program cannot possibly handle', 'test2@test.com', 'Test123_') is False
+    # different variations of allowed names
+    assert register('u 0', 'user2@test.com', 'Test123_') is True
+    assert register('u      0', 'user3@test.com', 'Test123_') is True
+    assert register('TEST 0', 'user4@test.com', 'Test123_') is True
+    assert register('Test', 'user5@test.com', 'Test123_') is True
+
+    # testing for email in this block
+    assert register('u0', 'test0@test.com', 'Test123_') is True
+    assert register('u0', 'test1@test.com', 'Test123_') is True
+    # repeated email
+    assert register('u0', 'test0@test.com', 'Test123_') is False
+    # illegal email (missing ".")
+    assert register('u0', 'test1@testcom', 'Test123_') is False
+    # illegal email (multiple @)
+    assert register('u0', 'test1@@test.com', 'Test123_') is False
+    # illegal email (missing domain after @)
+    assert register('u0', 'test1@.com', 'Test123_') is False
+    # empty email
+    assert register('u0', '', 'Test123_') is False
+    # illegal email (missing content before @)
+    assert register('u0', '@test.com', 'Test123_') is False
+    # one that works to prove username and password is valid
+    assert register('u0', 'test3@test.com', 'Test123_') is True
+
+    # Testing password
+    assert register('u0', 'p0@test.com', 'Test123_') is True
+    # empty password (falls under password too short)
+    assert register('u0', 'p1@test.com', '') is False
+    # missing special character
+    assert register('u0', 'p1@test.com', 'Test123') is False
+    # missing number
+    assert register('u0', 'p1@test.com', 'Test_') is False
+    # missing number and special character
+    assert register('u0', 'p1@test.com', 'Test') is False
+    # does not have upper case letter
+    assert register('u0', 'p1@test.com', 'test_dsak123') is False
+    # does not have lower case letter
+    assert register('u0', 'p1@test.com', 'TEST123_') is False
+    # one that works to prove username and email is valid
+    assert register('u0', 'p1@test.com', 'TESTpassword_123*^%') is True
+
+
+def test_r2_1_login():
+    """
+    Testing R2-1: A user can log in using her/his email address
+      and the password.
+    (will be tested after the previous test,
+    so we already have many users in database)
+    """
+
+    user = login('user0@test.com', 'Test123_')
+    assert user is not None
+    assert user.username == 'u0'
+
+    user = login('test0@test.com', '1234567')
+    assert user is None
+
+
+test_r1_7_user_register()
+test_r2_1_login()
