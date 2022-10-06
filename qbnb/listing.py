c9968b62010b18349658c3ecee28fb5cc18e7bc0
Connor <connordecan@gmail.com>
2022-10-06 00:01:24 -0400

    Added test code for the listing functions, implemented the listing functions and commented out the sample code to prevent conflicts

--- qbnb/listing.py
+++ qbnb/listing.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from qbnb.user import User
+from datetime import date
+from qbnb import app
 
 # setting up SQLAlchemy and data models
 # so we can map data models into database tables
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 
@@ -16,30 +17,37 @@ class Listing(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=True)
     price = db.Column(db.Float, nullable=False)
-    last_modified_date = db.Column(db.Date, nullable=False)
-    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'),
-                         nullable=False)
-    owner = db.relationship('Listing', backref=db.backref('listings',
-                                                          lazy=True))
+    last_modified_date = db.Column(db.Date, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey(User.id),
+                         nullable=True)
+    # owner = db.relationship('Listing', backref=db.backref('listings',
+    #                                                     lazy=True))
 
     def __repr__(self):
         """A listing represents itself by displaying the associated title"""
         return '<Listing %r>' % self.title
 
 
+db.create_all()
+
+
 def create_listing(owner_id, title, description, price):
     # get the user posting the listing
     user = User.query.get(owner_id)
+    if not user:
+        return False
     # check if user email exists
-    email_existed = User.query.filter_by(user=user, email=user.email).all()
-    if len(email_existed) > 0:
+    email_existed = User.query.filter_by(email=user.email).all()
+    if not len(email_existed) > 0:
+        return False
+    if user.email == "":
         return False
     # check if title meets criteria:
     if len(title) < 1 or len(title) > 80:
         return False
     if not title.isalnum() or title.endswith(' ') or title.startswith(' '):
         return False
-    title_existed = Listing.query.filter_by(title=title)
+    title_existed = Listing.query.filter_by(title=title).all()
     if len(title_existed) > 0:
         return False
     # check if description meets criteria:
@@ -55,4 +63,6 @@ def create_listing(owner_id, title, description, price):
     # listing is ok -> make listing
     listing = Listing(title=title, description=description, price=price,
                       last_modified_date=last_modified_date, owner_id=owner_id)
-
+    db.session.add(listing)
+    db.session.commit()
+    return True
--- qbnb_test/test_listing.py
+++ qbnb_test/test_listing.py
@@ -1,25 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from qbnb import app
 from qbnb.listing import create_listing
-from qbnb.models import User
+from qbnb.user import User
+from qbnb.listing import Listing
 
-
+db = SQLAlchemy(app)
+user1 = User(id=1, email="test@test.com", password="asdasd", username="asdasd", billing_address="asdasd", postal_code="asdasd", balance=5)
+user2 = User(id=2, email="", password="password", username="username2", billing_address="asdasd", postal_code="asdasd", balance=5)
+db.session.add(user1)
+db.session.add(user2)
+db.session.commit()
 def test_r4_1_create_listing():
     '''
     Testing R4-1: If title is not alphanumeric or has
     spaces at the front or back,then it fails.
     '''
-
-    user = User.user(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'TestOne', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Title2', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test1', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Title!', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test1!', 'DescriptionDescription', 20.00)\
            is False
-    assert create_listing('1', 'Title ', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test1 ', 'DescriptionDescription', 20.00)\
            is False
-    assert create_listing('1', ' Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, ' Test1', 'DescriptionDescription', 20.00)\
            is False
-    assert create_listing('1', ' Title ', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, ' Test1 ', 'DescriptionDescription', 20.00)\
            is False
 
 
@@ -28,11 +34,9 @@ def test_r4_2_create_listing():
     Testing R4-2: If the title of the product is longer than 80 characters
      then it fails.
     '''
-
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test2', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    assert create_listing(1, 'Titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
                                'eeeeee'
                                'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
                                'eeeeeeeeeeeeee',
@@ -48,14 +52,13 @@ def test_r4_3_create_listing():
      then it fails
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test3', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Title2', 'Description', 20.00) is False
+    assert create_listing(1, 'Test32', 'Description', 20.00) is False
     longword = "a"
     for i in range(3000):
         longword += 'a'
-    assert create_listing('1', 'Title3', longword, 20.00) is False
+    assert create_listing(1, 'Test33', longword, 20.00) is False
 
 
 def test_r4_4_create_listing():
@@ -63,10 +66,9 @@ def test_r4_4_create_listing():
     Testing R4-4: If description is shorter than product's title, it fails
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test4', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'TitleTitleTitleTitleTitleTitle'
+    assert create_listing(1, 'TitleTitleTitleTitleTitleTitle'
                                'TitleTitleTitle',
                           'DescriptionDescription', 20.00) is False
 
@@ -76,12 +78,11 @@ def test_r4_5_create_listing():
     Testing R4-5: If price is lower than 10 or higher than 10,000, it fails
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test5', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Title2', 'DescriptionDescription', 9.00)\
+    assert create_listing(1, 'Test52', 'DescriptionDescription', 9.00)\
            is False
-    assert create_listing('1', 'Title3', 'DescriptionDescription',
+    assert create_listing(1, 'Test53', 'DescriptionDescription',
                           10001.00) is False
 
 
@@ -91,8 +92,7 @@ def test_r4_6_create_listing():
      and before 2025-01-02, it fails
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test6', 'DescriptionDescription', 20.00)\
            is True
 
 
@@ -101,13 +101,11 @@ def test_r4_7_create_listing():
     Testing R4-7: The corresponding email cannot be empty and must exist
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    user2 = User(user_id=2, email="")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test7', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('2', 'Title2', 'DescriptionDescription', 20.00)\
+    assert create_listing(2, 'Test72', 'DescriptionDescription', 20.00)\
            is False
-    assert create_listing('3', 'Title3', 'DescriptionDescription', 20.00)\
+    assert create_listing(3, 'Test73', 'DescriptionDescription', 20.00)\
            is False
 
 
@@ -116,8 +114,7 @@ def test_r4_8_create_listing():
     Testing r4-8: Two listings cannot have the same title
     '''
 
-    user = User(user_id=1, email="test@test.com")
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test8', 'DescriptionDescription', 20.00)\
            is True
-    assert create_listing('1', 'Title', 'DescriptionDescription', 20.00)\
+    assert create_listing(1, 'Test8', 'DescriptionDescription', 20.00)\
            is False
