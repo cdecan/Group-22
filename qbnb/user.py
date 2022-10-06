bc64e1ed0685141c715af95b402518dfb78b936b
genglin6688 <genglin666888@gmail.com>
2022-10-06 12:11:42 -0400

    added comments to all attributes of user entity

--- qbnb/user.py
+++ qbnb/user.py
@@ -12,15 +12,21 @@ class User(db.Model):
     """
     Contains info about the user data
     """
-    # Basic user info
+    # Auto generated user id that is unique
     id = db.Column(db.Integer, primary_key=True,
                    unique=True, nullable=False)
+    # user email, cannot be null
     email = db.Column(db.String(120), unique=True, nullable=False)
+    # password of user, cannot be null
     password = db.Column(db.String(120), nullable=False)
+    # username of user, cannot be null
     username = db.Column(db.String(80), unique=True, nullable=False)
+    # billing_address, null only for sprint 2
     billing_address = db.Column(db.String(120))
+    # postal code of user, null only for sprint 2
     postal_code = db.Column(db.String(120))
-    balance = db.Column(db.Integer, nullable=True)
+    # balance of user, cannot be null
+    balance = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User %s>' % self.id
