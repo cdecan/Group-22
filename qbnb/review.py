376863e626038d9c65822ae8526a5b6cd7f8515a
Kenneract <37283040+Kenneract@users.noreply.github.com>
2022-10-06 22:26:13 -0400

    Switched to using qbnb's flask app object

--- qbnb/review.py
+++ qbnb/review.py
@@ -1,11 +1,10 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from qbnb import app
 
 # Setting up SQLAlchemy and data models so we can map data models into
 # database tables
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
+db.create_all()
 
 
 class Review(db.Model):
