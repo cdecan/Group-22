0f39807af9bd4ee9902a0bf753d7f356c1cbb876
Kenneract <37283040+Kenneract@users.noreply.github.com>
2022-10-06 22:22:55 -0400

    Updated character-case of review import statement

--- qbnb/__main__.py
+++ qbnb/__main__.py
@@ -1,6 +1,6 @@
 from qbnb import *
 from qbnb.models import *
 from qbnb.Listing import *
-from qbnb.Review import *
+from qbnb.review import *
 from qbnb.Transaction import *
 from qbnb.User import *
