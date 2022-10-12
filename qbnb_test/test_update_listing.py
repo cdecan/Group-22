b942c503869740a3481f64301220f7bd30179336
Haiden <haiden.m.smith02@gmail.com>
2022-10-11 23:18:51 -0400

    Corrected conflicts with database usage

--- qbnb_test/test_update_listing.py
+++ qbnb_test/test_update_listing.py
@@ -7,60 +7,51 @@ from qbnb import app
 
 db = SQLAlchemy(app)
 
-user1 = User(id=1,
-             email="test@test.com",
+user1 = User(email="test_2@test.com",
              password="Password_",
              username="User1",
              billing_address="",
              postal_code="",
              balance=100)
-user2 = User(id=2,
-             email="",
-             password="Password_",
-             username="User2",
-             billing_address="",
-             postal_code="",
-             balance=100)
 db.session.add(user1)
-db.session.add(user2)
 
 test_date = datetime.datetime(2021, 8, 5).date()
-test_listing_1 = Listing(id=1,
+test_listing_1 = Listing(id=11,
                          title="listing 1",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
                          owner_id=1)
-test_listing_2 = Listing(id=2,
+test_listing_2 = Listing(id=12,
                          title="listing 2",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
                          owner_id=1)
-test_listing_3 = Listing(id=3,
+test_listing_3 = Listing(id=13,
                          title="listing 3",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
                          owner_id=1)
-test_listing_4 = Listing(id=4,
+test_listing_4 = Listing(id=14,
                          title="listing 4",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
                          owner_id=1)
-test_listing_5 = Listing(id=5,
+test_listing_5 = Listing(id=15,
                          title="listing 5",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
                          owner_id=2)
-test_listing_6 = Listing(id=6,
+test_listing_6 = Listing(id=16,
                          title="listing 6",
                          description="description",
                          price=20.00,
                          last_modified_date=test_date,
-                         owner_id=3)
+                         owner_id=999)
 db.session.add(test_listing_1)
 db.session.add(test_listing_2)
 db.session.add(test_listing_3)
@@ -76,82 +67,82 @@ def test_r5_1_update_listing():
         listing, except owner_id and last_modified_date.
     """
 
-    assert update_listing(1,
-                          7,
+    assert update_listing(11,
+                          50,
                           None,
                           "this is a correct description",
                           21.00) is True
-    assert update_listing(7,
-                          1,
+    assert update_listing(50,
+                          11,
                           None,
                           "this is a correct description",
                           None) is True
-    assert update_listing(1,
-                          7,
+    assert update_listing(11,
+                          50,
                           None,
                           None,
                           22.00) is True
-    assert update_listing(7,
-                          1,
+    assert update_listing(50,
+                          11,
                           None,
                           None,
                           None) is True
-    assert update_listing(1,
-                          7,
+    assert update_listing(11,
+                          50,
                           "correct title 1 0",
                           "this is a correct description",
                           23.00) is True
-    assert update_listing(7,
-                          1,
+    assert update_listing(50,
+                          11,
                           "correct title 1 1",
                           "this is a correct description",
                           None) is True
-    assert update_listing(1,
-                          7,
+    assert update_listing(11,
+                          50,
                           "correct title 1 2",
                           None,
                           24.00) is True
-    assert update_listing(7,
-                          1,
+    assert update_listing(50,
+                          11,
                           "correct title 1 3",
                           None,
                           None) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           None,
                           "this is a correct description",
                           25.00) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           None,
                           "this is a correct description",
                           None) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           None,
                           None,
                           26.00) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           None,
                           None,
                           None) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           "correct title 1 4",
                           "this is a correct description",
                           27.00) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           "correct title 1 5",
                           "this is a correct description",
                           None) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           "correct title 1 6",
                           None,
                           28.00) is True
-    assert update_listing(1,
+    assert update_listing(11,
                           None,
                           "correct title 1 7",
                           None,
@@ -163,9 +154,9 @@ def test_r5_2_update_listing():
         Testing R5-2: Testing that the price can only be increased.
     """
 
-    assert update_listing(2, None, None, None, 19.00) is False
-    assert update_listing(2, None, None, None, 20.00) is False
-    assert update_listing(2, None, None, None, 21.00) is True
+    assert update_listing(12, None, None, None, 19.00) is False
+    assert update_listing(12, None, None, None, 20.00) is False
+    assert update_listing(12, None, None, None, 21.00) is True
 
 
 def test_r5_3_update_listing():
@@ -174,12 +165,12 @@ def test_r5_3_update_listing():
         updated when the update operation is successful.
     """
 
-    update_listing(3, None, None, None, 19.00)
-    check_date = Listing.query.filter_by(id=3).first()
+    update_listing(13, None, None, None, 19.00)
+    check_date = Listing.query.filter_by(id=13).first()
     assert check_date.last_modified_date == test_date
-    update_listing(3, None, None, None, 25.00)
-    check_date = Listing.query.filter_by(id=3).first()
-    assert check_date != test_date
+    update_listing(13, None, None, None, 25.00)
+    check_date = Listing.query.filter_by(id=13).first()
+    assert check_date.last_modified_date != test_date
     # assert tf is False
 
 
@@ -190,27 +181,27 @@ def test_r5_4_update_listing():
     """
 
     # R4-1 and R4-2
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           "correct title 4",
                           None,
                           None) is True
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           " incorrect title 4",
                           None,
                           None) is False
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           " incorrect title 4 ",
                           None,
                           None) is False
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           "incorrect title 4 ",
                           None,
                           None) is False
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           "incorrect * title 4",
                           None,
@@ -218,19 +209,19 @@ def test_r5_4_update_listing():
     title_over_80 = ""
     for i in range(85):
         title_over_80 += 'a'
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           title_over_80,
                           None,
                           None) is False
 
     # R4-3 and R4-4
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           None,
                           "This ! is @ a # correct $ description.",
                           None) is True
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           "T",
                           "Desc",
@@ -238,34 +229,35 @@ def test_r5_4_update_listing():
     desc_over_2000 = ""
     for i in range(2005):
         desc_over_2000 += 'a'
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           None,
                           desc_over_2000,
                           None) is False
-    assert update_listing(4,
+    assert update_listing(14,
                           None,
                           "This title is greater than 20 characters long",
                           "Description is shorter than title",
                           None) is False
 
     # R4-5
-    assert update_listing(4, None, None, None, 5.00) is False
-    assert update_listing(4, None, None, None, 10001.00) is False
+    assert update_listing(14, None, None, None, 5.00) is False
+    assert update_listing(14, None, None, None, 10001.00) is False
 
     # R4-6
-    temp = test_listing_4.last_modified_date
+    temp = Listing.query.filter_by(id=14).first()
+    temp = temp.last_modified_date
     temp = temp.strftime("%Y-%m-%d")
     temp = temp.replace('-', '')
     temp = int(temp)
     assert 20210102 < temp < 20250102
 
     # R4-7
-    assert update_listing(5, None, None, None, 25.00) is False
-    assert update_listing(6, None, None, None, 25.00) is False
+    assert update_listing(15, None, None, None, 25.00) is False
+    assert update_listing(16, None, None, None, 25.00) is False
 
     # R4-8
-    assert update_listing(4, None, "listing 2", None, None) is False
+    assert update_listing(14, None, "listing 2", None, None) is False
 
 
 db.drop_all()
