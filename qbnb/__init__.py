0476bafa110719681e94930ede95b3a1b31e7ae6
Connor <connordecan@gmail.com>
2022-10-17 22:05:46 -0400

    updated template

--- qbnb/__init__.py
+++ qbnb/__init__.py
@@ -2,6 +2,8 @@
 an init file is required for this folder to be considered as a module
 '''
 from flask import Flask
+from qbnb.cli import login_page, register_page
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
--- qbnb/__main__.py
+++ qbnb/__main__.py
@@ -2,5 +2,29 @@ from qbnb import *
 from qbnb.models import *
 from qbnb.listing import *
 from qbnb.review import *
-from qbnb.Transaction import *
+from qbnb.booking import *
 from qbnb.user import *
+from qbnb.cli import login_page, register_page
+
+
+def main():
+    while True:
+        selection = input(
+            'Welcome. Please type 1 to login. '
+            'Or type 2 register. Or type 3 to exit')
+        selection = selection.strip()
+        if selection == '1':
+            user = login_page()
+            if user:
+                print(f'welcome {user.username}')
+                break
+            else:
+                print('login failed')
+        elif selection == '2':
+            register_page()
+        elif selection == '3':
+            break
+
+
+if __name__ == '__main__':
+    main()
