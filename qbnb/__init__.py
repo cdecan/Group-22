efe35da4bae8b06da2c6ee52cc6a368b6d7bab37
Connor <connordecan@gmail.com>
2022-10-18 22:38:08 -0400

    commented out template and deleted old commented out template

--- qbnb/__init__.py
+++ qbnb/__init__.py
@@ -2,7 +2,7 @@
 an init file is required for this folder to be considered as a module
 '''
 from flask import Flask
-# from qbnb.cli import login_page, register_page
+# From qbnb.cli import login_page, register_page
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
