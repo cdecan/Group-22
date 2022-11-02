b67cbb84ec3ef7c587dada5effa7c7947b9daa41
genglin6688 <genglin666888@gmail.com>
2022-11-01 21:52:37 -0400

    reverted changes

--- qbnb_test/frontend/test_update_user_profile/test_update_user_profile.py
+++ qbnb_test/frontend/test_update_user_profile/test_update_user_profile.py
@@ -1,20 +1,6 @@
 from os import popen
 from pathlib import Path
 import subprocess
-from flask_sqlalchemy import SQLAlchemy
-from qbnb import app
-from qbnb.listing import create_listing
-from qbnb.user import User
-
-db = SQLAlchemy(app)
-user1 = User(email="email@email.com", password="Test123_",
-             username="User1", billing_address="",
-             postal_code="", balance=100)
-user2 = User(email="email1@email.com", password="Test123_", username="User2",
-             billing_address="", postal_code="", balance=100)
-db.session.add(user1)
-db.session.add(user2)
-db.session.commit()
 
 # get expected input/output file
 current_folder = Path(__file__).parent
@@ -41,4 +27,3 @@ def test_login():
 
     print('outputs', output)
     assert output.strip() == expected_out.strip()
-
