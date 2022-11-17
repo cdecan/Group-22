b6786d5ba6da7bdb33486600895dc710c9862b9f
Haiden Smith <haiden.m.smith02@gmail.com>
2022-11-16 19:20:12 -0500

    Added SQL injection test cases for the user_id attribute of the create_listing function. Added test_db to pytest.yml file.

--- qbnb_test/sql/test_db.py
+++ qbnb_test/sql/test_db.py
@@ -1,4 +1,4 @@
-from qbnb.user import register
+from qbnb.listing import create_listing
 from pathlib import Path
 
 current_folder = Path(__file__).parent
@@ -9,11 +9,14 @@ with open(current_folder.joinpath(
 print(test_lines)
 
 
-def test_register_email():
+def test_create_listing_owner_id():
     """A test to make sure that there are no injections
-       in the email attribute of register()
+       in the owner_id attribute of create_listing()
     """
 
     # make sure there are no errors thrown
     for line in test_lines:
-        register('User', line, 'Password1_')
+        create_listing(line,
+                       'Listing1',
+                       'This is a description of Listing1',
+                       50.0)
