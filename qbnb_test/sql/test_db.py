72389ec540da5c0bf4afb030f8c5022186fe4995
Kenneract <37283040+Kenneract@users.noreply.github.com>
2022-11-16 21:58:05 -0500

    Ensured the used userID always exists, and made sure each listing has a unique title

--- qbnb_test/sql/test_db.py
+++ qbnb_test/sql/test_db.py
@@ -1,4 +1,5 @@
 from qbnb.listing import create_listing
+from qbnb.user import register
 from pathlib import Path
 
 current_folder = Path(__file__).parent
@@ -13,11 +14,14 @@ def test_create_listing_description():
     """
     A test to ensure there are no SQL-injection vulnerabilities in
     the description argument of the create_listing() function.
+
+    Passes if no exceptions are thrown.
     """
+    # Ensure there is at least one user (user ID 1 exists)
+    register('User', 'SQLListing1@email.com', 'Testing#100')
     # Attempt various injection attacks into description field
-    # Passes if no exceptions are thrown.
-    id = 0
+    x = 0
     for line in test_lines:
-        create_listing(id, "This is a valid listing title",
+        create_listing(1, f"This is the listing title {x}",
                        line, 100)
-        id += 1  # (ensure owner_id is always unique)
+    x += 1
