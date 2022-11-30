e1113a1978c5bf073ab5bc12238af703fe1ac417
genglin6688 <genglin666888@gmail.com>
2022-11-29 19:57:06 -0500

    Created branch test_booking_frontend

---
+++ qbnb_test/frontend/test_booking/test_booking.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import subprocess
+from qbnb.listing import Listing
+from qbnb.user import User
+from datetime import datetime
+
+
+def test_booking():
+    """
+    """
+    # get expected input/output file
+    current_folder = Path(__file__).parent
+
+    # read expected in/out
+    expected_in = open(current_folder.joinpath(
+        'test_booking.in'))
+    expected_out = open(current_folder.joinpath(
+        'test_booking.out'), newline='\r\n').read()
+
+    # pip the input
+    output = subprocess.run(
+        ['python', '-m', 'qbnb'],
+        stdin=expected_in,
+        capture_output=True,
+    ).stdout.decode()
+
+    # make assertion for in/out
+    print('outputs', output)
+    assert output.strip() == expected_out.strip()
