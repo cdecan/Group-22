b277ed1eae9c83cd18f79b255cf5493285680676
Haiden <haiden.m.smith02@gmail.com>
2022-11-30 14:55:39 -0500

    Finished creating test cases for the backend of booking.

--- qbnb_test/backend/test_booking.py
+++ qbnb_test/backend/test_booking.py
@@ -29,30 +29,30 @@ create_listing(1,
                "listing 4",
                "This is a description of listing 4",
                10.00)
+create_listing(1,
+               "listing 5",
+               "This is a description of listing 5",
+               10.00)
+create_listing(1,
+               "listing 6",
+               "This is a description of listing 6",
+               10.00)
 test_date1 = datetime.datetime(2021, 8, 5)
-test_listing_5 = Listing(id=5,
-                         title="listing 5",
-                         description="This is a description of Listing 5",
+test_listing_7 = Listing(id=7,
+                         title="listing 7",
+                         description="This is a description of Listing 7",
                          price=20000.00,
                          last_modified_date=test_date1,
                          owner_id=1)
-test_listing_6 = Listing(id=6,
-                         title="listing 6",
-                         description="This is a description of Listing 6",
+test_listing_8 = Listing(id=8,
+                         title="listing 8",
+                         description="This is a description of Listing 8",
                          price=2.00,
                          last_modified_date=test_date1,
                          owner_id=1)
-db.session.add(test_listing_5)
-db.session.add(test_listing_6)
+db.session.add(test_listing_7)
+db.session.add(test_listing_8)
 db.session.commit()
-create_listing(1,
-               "listing 7",
-               "This is a description of listing 7",
-               10.00)
-create_listing(1,
-               "listing 8",
-               "This is a description of listing 8",
-               10.00)
 
 
 def test_r1():
@@ -64,37 +64,38 @@ def test_r1():
 
 def test_r2():
     """
-        Testing R2: A user cannot book a listing for his/her listing.
+        Testing R2: A user cannot book a listing
+                    for his/her listing.
     """
     assert book_listing(1, 2) is False
 
 
 def test_r3():
     """
-        Testing R3: A user cannot book a listing
-        that costs more than his/her balance.
+        Testing R3: A user cannot book a listing that
+                    costs more than his/her balance.
     """
     assert book_listing(3, 3) is False
 
 
 def test_r4():
     """
-        Testing R4: A user cannot book a listing that
-        is already booked with the overlapped dates.
+        Testing R4: A user cannot book a listing that is already
+                    booked with the overlapped dates.
     """
     assert book_listing(3, 1) is False
 
 
 def test_r6():
     """
-        Testing R6: User id exists in the database
+        Testing R6: User id exists in the database.
     """
     assert book_listing(6, 4) is False
 
 
 def test_r7():
     """
-        Testing R7: Listing id exists in the database
+        Testing R7: Listing id exists in the database.
     """
     assert book_listing(4, 10) is False
 
@@ -103,15 +104,16 @@ def test_r8():
     """
         Testing R8: Price has to be of range [10, 10000].
     """
-    assert book_listing(4, 5) is False
-    assert book_listing(4, 6) is False
+    assert book_listing(4, 7) is False
+    assert book_listing(4, 8) is False
 
 
 def test_r9():
     """
-        Testing R9: Date must be after 2021-01-02 and before 2025-01-02
+        Testing R9: Date must be after 2021-01-02
+                    and before 2025-01-02.
     """
-    assert book_listing(4, 7) is True
+    assert book_listing(4, 5) is True
     temp = Booking.query.filter_by(id=2).first()
     temp = temp.date
     lower_bound = datetime.datetime(2021, 1, 2, 23, 59, 59)
@@ -121,12 +123,13 @@ def test_r9():
 
 def test_r10():
     """
-        Testing R10: User balance is correctly updated upon successful booking
+        Testing R10: User balance is correctly updated
+                     upon successful booking.
     """
     temp = User.query.filter_by(id=5).first()
     balance_before = temp.balance
 
-    assert book_listing(5, 8) is True
+    assert book_listing(5, 6) is True
 
     temp = User.query.filter_by(id=5).first()
     balance_after = temp.balance
